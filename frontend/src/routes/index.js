2295155ad3697bca64ef44909827cff25868f795
fullToStack <fulltostack@gmail.com>
2020-03-19 20:36:40 +0530

    Removed no metamask detected screen

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Main from '../containers/Main';
-import MetaMaskNotDetected from '../containers/MetaMaskNotDetected';
 
 const routes = (
   <Switch>
     <Route exact path="/" component={Main} />
-    <Route path="/no-metamask" component={MetaMaskNotDetected} />
     <Redirect from="/*" to="/" />
   </Switch>
 );
