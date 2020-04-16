324a3fb284d0407374733d9eb6032fbf654abd37
fullToStack <fulltostack@gmail.com>
2020-04-16 21:18:28 +0530

    Add history to routes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,13 +3,17 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Main from '../containers/Main';
 import MirrorSwapContract from '../containers/MirrorSwapContract';
 import Fill from '../components/Fill';
+import { history } from '../app/store';
+import { ConnectedRouter } from 'connected-react-router';
 
 const routes = (
   <Switch>
-    <Route exact path="/swap" component={Main} />
-    <Route exact path="/fill" component={Fill} />
-    <Route exact path="/fill/:contractAddress" component={MirrorSwapContract} />
-    <Redirect from="/*" to="/swap" />
+    <ConnectedRouter history={history}>
+      <Route exact path="/swap" component={Main} />
+      <Route exact path="/fill" component={Fill} />
+      <Route exact path="/fill/:contractAddress" component={MirrorSwapContract} />
+      <Redirect from="/*" to="/swap" />
+    </ConnectedRouter>
   </Switch>
 );
 
