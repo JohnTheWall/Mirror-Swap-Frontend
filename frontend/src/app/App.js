5da5947acedb6ace28529818e451b7dcabb939d1
fullToStack <fulltostack@gmail.com>
2020-04-16 21:17:58 +0530

    Refactor app component

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import routes from '../routes';
 import { startSaga } from './rootSaga';
 import { connect } from 'react-redux';
@@ -6,49 +6,23 @@ import Snackbars from '../components/SnackBars';
 import { setClose } from '../containers/Notifications/reducer';
 import { connectMetamask } from '../containers/Metamask/reducer'
 import { push } from 'connected-react-router'
-import { Grid, Box, makeStyles } from '@material-ui/core';
+import { Grid, Box } from '@material-ui/core';
 import NavigationTabs from '../components/NavigationTabs';
 import { BrowserRouter } from 'react-router-dom';
 import Header from '../components/Header';
-import styled from 'styled-components'
-
-const useStyles = makeStyles(theme => ({
-  swapButtonContainer: {
-    padding: '10px',
-    display: 'table',
-    margin: 'auto',
-  },
-  swapButton: {
-    background: '#eee',
-    color: '#000 !important',
-  },
-  customPaper: {
-    padding: theme.spacing(2),
-  },
-}));
-
-const HeaderWrapper = styled.div`
-  ${({ theme }) => theme.flexRowNoWrap}
-  width: 100%;
-  justify-content: space-between;
-`
 
 const App = (props) => {
-  const { setClose, isAvailable, connectMetamask, user } = props
-  const [isDialogOpen, setIsDialogOpen] = useState(false)
-  const classes = useStyles();
-
+  const tabs = [{ label: "Swap", path: "/swap" }, { label: "Fill", path: "/fill" }]
   return (
     <React.Fragment>
       <Snackbars {...props.notifications} setClose={setClose} />
-      <HeaderWrapper>
-        <Header />
-      </HeaderWrapper>
+      <Header />
       <Box m={4}>
         <Grid container justify="center">
           <Grid item xs={10} sm={5}>
             <BrowserRouter>
-              <NavigationTabs />
+              <NavigationTabs
+                tabs={tabs} />
               {routes}
             </BrowserRouter>
           </Grid>
