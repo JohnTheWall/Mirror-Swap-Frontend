69d0b365555edb157515e173e6f52d14f38fba38
fullToStack <fulltostack@gmail.com>
2020-04-16 21:17:14 +0530

    Refactor components useStyle via material ui

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,104 +1,32 @@
 import React from 'react'
-import styled from 'styled-components'
+import Typography from '@material-ui/core/Typography';
+import { Grid, makeStyles, Button } from '@material-ui/core';
 
-import { Link } from '../../theme'
-// import Web3Status from '../Web3Status'
-import { darken } from 'polished'
-
-const HeaderFrame = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  margin-top: 23px;
-`
-
-const HeaderElement = styled.div`
-  margin: 1.25rem;
-  display: flex;
-  min-width: 0;
-  display: flex;
-  align-items: center;
-`
-
-const Nod = styled.span`
-  transform: rotate(0deg);
-  transition: transform 150ms ease-out;
-
-  :hover {
-    transform: rotate(-10deg);
-  }
-`
-
-const Title = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 22px;
-
-  :hover {
-    cursor: pointer;
-  }
-
-  #link {
-    text-decoration-color: ${({ theme }) => theme.UniswapPink};
-  }
-
-  #title {
-    display: inline;
-    font-size: 1rem;
-    font-weight: 500;
-    color: ${({ theme }) => theme.wisteriaPurple};
-    :hover {
-      color: ${({ theme }) => darken(0.2, '#3f51b5')};
-    }
-  }
-`
-const CurrencySelect = styled.button`
-  align-items: center;
-  font-size: 1rem;
-  color: ${({ selected, theme }) => (selected ? theme.textColor : theme.royalBlue)};
-  height: 2rem;
-  border: 1px solid ${({ selected, theme }) => (selected ? theme.mercuryGray : theme.royalBlue)};
-  border-radius: 2.5rem;
-  background-color: white;
-  outline: none;
-  cursor: pointer;
-  user-select: none;
-  border-color: #d8d4d4;
-  float: right;
-  margin-right: 18px;
-  :hover {
-    border: 1px solid
-      ${({ selected, theme }) => (selected ? darken(0.2, '#3f51b5') : darken(0.2, '#3f51b5'))};
-      background-color: #1d3fff;
-      color: white;
-  }
-
-  :focus {
-    border: 1px solid ${({ theme }) => darken(0.2, '#3f51b5')};
-  }
-
-  :active {
-    background-color: ${({ theme }) => theme.zumthorBlue};
-  }
-`
+const useStyles = makeStyles(theme => ({
+  button: {
+    background: '#eee',
+    borderRadius: '2.5rem',
+    height: 'fit-content',
+    "&:hover, &:focus": {
+      background: '#0000FF',
+      color: '#FFFFFF'
+    },
+    alignContent: 'flex-end',
+    marginInlineStart: 'auto'
+  },
+    grid: {
+      padding: theme.spacing(3),
+      display: 'flex',
+    },
+  }));
 
 const Header = () => {
+  const classes = useStyles()
   return (
-    <HeaderFrame>
-      <Title>
-        <Nod>
-          <Link id="link" href="/#">
-          </Link>
-        </Nod>
-        <Link id="link" href="/#">
-          <h1 id="title">Mirror Swap</h1>
-        </Link>
-      </Title>
-      <CurrencySelect>
-        Connect Account
-        </CurrencySelect>
-    </HeaderFrame>
+    <Grid className={classes.grid} xs={12}>
+      <Typography variant="h6">Mirror Swap</Typography>
+      <Button className={classes.button}>Connect Account</Button>
+    </Grid>
   )
 }
 
--- frontend/src/components/NavigationTabs/index.js
+++ frontend/src/components/NavigationTabs/index.js
@@ -1,165 +1,45 @@
-import React, { useCallback } from 'react'
-import { withRouter, NavLink } from 'react-router-dom'
-import styled from 'styled-components'
-import { transparentize, darken } from 'polished'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { makeStyles } from '@material-ui/core';
+import { push } from 'connected-react-router';
 
-const tabOrder = [
-  {
-    path: '/swap',
-    textKey: 'Swap',
-    regex: /\/swap/
-  },
-  {
-    path: '/fill',
-    textKey: 'Fill',
-    regex: /\/fill/
-  },
-]
-
-const BetaMessage = styled.div`
-  ${({ theme }) => theme.flexRowNoWrap}
-  cursor: pointer;
-  flex: 1 0 auto;
-  align-items: center;
-  position: relative;
-  padding: 0.5rem 1rem;
-  padding-right: 2rem;
-  margin-bottom: 1rem;
-  border: 1px solid ${({ theme }) => transparentize(0.6, theme.wisteriaPurple)};
-  background-color: ${({ theme }) => transparentize(0.9, theme.wisteriaPurple)};
-  border-radius: 1rem;
-  font-size: 0.75rem;
-  line-height: 1rem;
-  text-align: left;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  color: ${({ theme }) => theme.wisteriaPurple};
-
-  &:after {
-    content: '✕';
-    top: 0.5rem;
-    right: 1rem;
-    position: absolute;
-    color: ${({ theme }) => theme.wisteriaPurple};
-  }
-`
-
-const DaiMessage = styled(BetaMessage)`
-  ${({ theme }) => theme.flexColumnNoWrap}
-  position: relative;
-  word-wrap: wrap;
-  overflow: visible;
-  white-space: normal;
-  padding: 1rem 1rem;
-  padding-right: 2rem;
-  line-height: 1.2rem;
-  cursor: default;
-  color: ${({ theme }) => theme.textColor};
-  div {
-    width: 100%;
-  }
-  &:after {
-    content: '';
-  }
-`
-
-const CloseIcon = styled.div`
-  width: 10px !important;
-  top: 0.5rem;
-  right: 1rem;
-  position: absolute;
-  color: ${({ theme }) => theme.wisteriaPurple};
-  :hover {
-    cursor: pointer;
-  }
-`
-
-const WarningHeader = styled.div`
-  margin-bottom: 10px;
-  font-weight: 500;
-  color: ${({ theme }) => theme.uniswapPink};
-`
-
-const WarningFooter = styled.div`
-  margin-top: 10px;
-  font-size: 10px;
-  text-decoration: italic;
-  color: ${({ theme }) => theme.greyText};
-`
-
-const Tabs = styled.div`
-  ${({ theme }) => theme.flexRowNoWrap}
-  align-items: center;
-  height: 2.5rem;
-  background-color: #eee;
-  border-radius: 3rem;
-  /* border: 1px solid ${({ theme }) => theme.mercuryGray}; */
-  margin-bottom: 1rem;
-`
-
-const activeClassName = 'ACTIVE'
-
-const StyledNavLink = styled(NavLink).attrs({
-  activeClassName
-})`
-  ${({ theme }) => theme.flexRowNoWrap}
-  align-items: center;
-  justify-content: center;
-  height: 2.5rem;
-  border: 1px solid ${({ theme }) => theme.mercuryGray};
-  flex: 1 0 auto;
-  border-radius: 3rem;
-  outline: none;
-  cursor: pointer;
-  text-decoration: none;
-  box-sizing: border-box;
-  width: 50%;
-  display: inline-flex;
-  font-size: large;
-  border-color: aliceblue;
-  color: black;
-
-  &.${activeClassName} {
-    background: #3f51b5;
-    color: white;
-
-    border-radius: 3rem;
-    border: 1px solid ${({ theme }) => theme.mercuryGray};
-    box-shadow: 0 4px 8px 0 ${({ theme }) => theme.shadowColor};
-    box-sizing: border-box;
-    font-weight: 500;
-    :hover {
-      /* border: 1px solid ${({ theme }) => theme.mercuryGray}; */
-      background-color: ${({ theme }) => theme.inputBackground};
-    }
-  }
-
-  :hover,
-  :focus {
-    color: ${({ theme }) => theme.royalBlue};
-  }
-`
-
-function NavigationTabs({ location: { pathname }, history }) {
-  const navigate = useCallback(
-    direction => {
-      const tabIndex = tabOrder.findIndex(({ regex }) => pathname.match(regex))
-      history.push(tabOrder[(tabIndex + tabOrder.length + direction) % tabOrder.length].path)
+const useStyles = makeStyles(theme => ({
+  tab: {
+    borderRadius: '2.5rem',
+    background: '#eee',
+    "&:hover, &:focus": {
+      background: '#0000FF',
+      color: '#FFFFFF'
     },
-    [pathname, history]
-  )
+  },
+  tabs: {
+    borderRadius: '2.5rem',
+    background: '#eee',
+  }
+}));
 
+function NavigationTabs(props) {
+  const classes = useStyles()
+  const handelClick = (path) => {
+    console.log(path)
+    push(path)
+  }
   return (
-    <>
-      <Tabs>
-        {tabOrder.map(({ path, textKey, regex }) => (
-          <StyledNavLink key={path} to={path} isActive={(_, { pathname }) => pathname.match(regex)}>
-            {textKey}
-          </StyledNavLink>
-        ))}
-      </Tabs>
-    </>
+    <Tabs
+      className={classes.tabs}
+      variant="fullWidth"
+    >
+      {
+        props.tabs.map(
+          ({ label, path }) => <Tab value={label}
+            label={label}
+            className={classes.tab}
+            onClick={() => handelClick(path)} />
+        )
+      }
+    </Tabs>
   )
 }
 
