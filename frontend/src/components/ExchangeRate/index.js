a629cccee582d7360a93d00bef7067805b7a8b45
fullToStack <fulltostack@gmail.com>
2020-04-29 23:10:24 +0530

    Refactor exchange rate

--- frontend/src/components/ExchangeRate/index.js
+++ frontend/src/components/ExchangeRate/index.js
@@ -8,11 +8,13 @@ const useStyles = makeStyles(theme => ({
     width: '95%',
     margin: 'auto',
     background: '#eee',
-    borderRadius: '0px 0px 15px 15px'
+    borderRadius: '0px 0px 15px 15px',
+    height: '8%'
   },
   customeTypography: {
     marginInlineStart: 'auto',
-    paddingRight: theme.spacing(1)
+    paddingRight: theme.spacing(1),
+    fontSize: '12px',
   },
   exchangeRate: {
     fontSize: 13,
@@ -20,15 +22,17 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const ExchangeRates = () => {
+const ExchangeRates = (props) => {
 
   const classes = useStyles()
   return (
     <Grid className={classes.customGrid}>
       <Typography className={classes.exchangeRate}>Exchange Rate:</Typography>
-      <Typography className={classes.customeTypography}>-</Typography>
+      <Typography className={classes.customeTypography}>
+        {props.rate ? `1 ${props.inputCurrency.symbol} = ${props.rate.toFixed(5)} ${props.outputCurrency.symbol}`: '-'}
+      </Typography>
     </Grid>
-  );  
+  );
 };
 
 ExchangeRates.propTypes = {
